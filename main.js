c8820be28de3f5d4e2cc1e1f5599c01c0db863df
Giovanni Cestaro <92190819+GiovanniCestaro@users.noreply.github.com>
2021-10-21 21:34:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const linksSocialMedia = {
+  github: 'giovannicestaro',
+  youtube: 'maykbrito',
+  facebook: 'giovanni.cestaro.7',
+  instagram: 'giovannicestaro',
+  twitter: 'maykbrito'
+}
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `http://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGithubProfileInfos()
